3265307246216fe9d83786c1640fe4b3aa0f6fc9
Alfredo Jeonghyun Park <parkalfredojeonghyun@gmail.com>
2021-04-05 18:10:55 -0500

    Implemented smooth scrolling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const header = document.querySelector(".header");
 const nav = document.querySelector(".navigation");
+const navLink = document.querySelectorAll(".navigation__link");
 
 const stickyNav = function (entries) {
 	const [entry] = entries;
@@ -12,6 +13,19 @@ const stickyNav = function (entries) {
 
 const observer = new IntersectionObserver(stickyNav, {
 	root: null,
-	threshold: 0,
+	threshold: 0.3,
 });
 observer.observe(header);
+
+navLink.forEach((el) => {
+	el.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		const id = this.getAttribute("href");
+		console.log(id);
+
+		document.querySelector(id).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
+});
